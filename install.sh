d2c9154247b5f6036091de176d5697acadd83e02
azubkov <anton.zubkov@hpe.com>
2023-08-28 18:33:45 +0300

    EZAF-3013: updated image tag to q4

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ APP_IMAGE_TAG=${APP_IMAGE_TAG:-"fy23-q4"}
 
 function build() {
   echo "Building the ${APP_IMAGE_NAME}:${APP_IMAGE_TAG} image"
-  docker build -t "${APP_IMAGE_NAME}":"${APP_IMAGE_TAG}" .
+  docker build --platform linux/amd64 -t "${APP_IMAGE_NAME}":"${APP_IMAGE_TAG}" .
 
   if [ $? -ne 0 ]; then
     echo "[ERROR]---: Build failed. Exiting ..."
