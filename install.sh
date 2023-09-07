6d19ba2b4cab13ce2aa9bf2f85bfe34a4839d236
Anton Zubkov <102999737+azubkov-swe@users.noreply.github.com>
2023-09-07 16:03:21 +0300

    EZAF-3013:  Update image tag to q4 (#36)
    * EZAF-3013: updated image tag to q4
    
    * EZAF-3013: updated image tag to q4

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 APP_IMAGE_NAME=${APP_IMAGE_NAME:-"gcr.io/mapr-252711/ezua-tutorials"}
-APP_IMAGE_TAG=${APP_IMAGE_TAG:-"fy23-q3-GA-08022023"}
+APP_IMAGE_TAG=${APP_IMAGE_TAG:-"fy23-q4"}
 
 function build() {
   echo "Building the ${APP_IMAGE_NAME}:${APP_IMAGE_TAG} image"
-  docker build -t "${APP_IMAGE_NAME}":"${APP_IMAGE_TAG}" .
+  docker build --platform linux/amd64 -t "${APP_IMAGE_NAME}":"${APP_IMAGE_TAG}" .
 
   if [ $? -ne 0 ]; then
     echo "[ERROR]---: Build failed. Exiting ..."
