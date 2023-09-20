59d619d6b0912d59c4c4dae4e7d36f91a899e85f
Dimitris Poulopoulos <dimitrios.poulopoulos@hpe.com>
2023-09-20 13:31:03 +0300

    question-answering: Amend the LLM predictor image
    Make the LLM predictor Docker image lighter:
    - Remove the LangChain dependency: LangChain is not needed anymore since
      it was only used as a wrapper around the GPT4ALL Python library. The
      predictor now uses the GPT4ALL library directly to generate text.
    - Remove the model: The model is not part of the Docker image anymore.
      The predictor is not responsible to download the model during runtime.
      This also permits us to change the model type (i.e., architecture)
      by passing an argument to the pod command (--model).
    
    These changes yield a lighter image that is faster to build and push.
    
    Refs #57
    
    Signed-off-by: Dimitris Poulopoulos <dimitrios.poulopoulos@hpe.com>

--- demos/question-answering/llm/src/model.py
+++ demos/question-answering/llm/src/model.py
@@ -4,50 +4,86 @@ import argparse
 
 import kserve
 
-from langchain.llms import GPT4All
-from langchain import LLMChain, PromptTemplate
+from gpt4all import GPT4All
 
 
 logger = logging.getLogger(__name__)
 
-LLM_PATH = 'ggml-gpt4all-l13b-snoozy.bin'
+MODEL = "orca-mini-3b.ggmlv3.q4_0.bin"
+
 PROMPT_TEMPLATE = """
-Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+### System:
+{system}
+
+### User:
+{instruction}
 
 {context}
 
-Question: {question}
-Answer:
+### Input:
+{input}
+
+Response:
+
 """
 
 
 class LLM(kserve.Model):
-    def __init__(self, name: str):
+    def __init__(self, name: str, model: str, model_path: str):
         super().__init__(name)
         self.name = name
-        self.llm = GPT4All(model=f"{os.getcwd()}/ggml-gpt4all-j-v1.3-groovy.bin",
-                           allow_download=True, temp=0.2)
+
+        logger.info(f"Initializing model: {model}")
+        self._init_model(model, model_path)
+
+        self.ready = True
+
+    def _init_model(self, model, model_path):
+        self.model = GPT4All(
+            model_name=model, model_path=model_path, allow_download=True)
 
     def predict(self, request: dict, headers: dict) -> dict:
-        data = request["instances"]
-        context = data[0]["context"]
-        question = data[0]["question"]
+        data = request["instances"][0]
+
+        # prompt parameters
+        prompt_template = data.get("prompt_template", PROMPT_TEMPLATE)
+        system = data.get("system", "")
+        instruction = data["instruction"]
+        context = data["context"]
+        query = data["input"]
+
+        logger.info(f"Received instruction: {instruction}")
+        logger.info(f"Received input: {query}")
+
+        # generation parameters
+        temperature = data.get("temperature", .2)
+        max_tokens = data.get("max_tokens", 200)
+        top_k = data.get("top_k", 40)
+        top_p = data.get("top_p", .4)
         
-        logger.info(f"Received context: {context}")
-        logger.info(f"Received question: {question}")
+        logger.info("Creating the prompt...")
+        prompt = prompt_template.format(
+            system=system, instruction=instruction,
+            context=context, input=query)
         
-        prompt = PromptTemplate(
-            template=PROMPT_TEMPLATE, input_variables=["context", "question"])
-        
-        chain = LLMChain(llm=self.llm, prompt=prompt)
-        response = chain.run({
-                       "context": context,
-                       "question": question})
+        response = self.model.generate(
+            prompt, max_tokens=max_tokens,
+            temp=temperature, top_k=top_k, top_p=top_p)
+
         logger.info(f"LLM response: {response}")
 
         return {"predictions": [response]}
 
 
 if __name__ == "__main__":
-    model = LLM("llm")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model", help="The name of the LLM model to use.",
+        default=MODEL)
+    parser.add_argument(
+        "--model_path", help="The path to the model.",
+        default=os.getcwd())
+    args = parser.parse_args()
+
+    model = LLM("llm", args.model, args.model_path)
     kserve.ModelServer(workers=1).start([model])
