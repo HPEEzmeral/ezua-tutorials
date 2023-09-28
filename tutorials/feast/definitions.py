76ffe4687e516aaafcb2f92141bcd1af25759ca0
Dimitris Poulopoulos <dimitrios.poulopoulos@hpe.com>
2023-09-28 08:49:57 +0000

    feast: Improve the Feast Ride Sharing tutorial
    Improve the Feast Ride Sharing tutorial by:
    - Updating the code to work with the latest version of the Feast
      Python client.
    - Extending the Notebook documentation and correcting syntax and
      grammar.
    - Using the training dataset ingested in Feast to train a simple model.
    - Simplifying the `definitions.py` file.
    
    Refs #62
    
    Signed-off-by: Dimitris Poulopoulos <dimitrios.poulopoulos@hpe.com>

--- tutorials/feast/definitions.py
+++ tutorials/feast/definitions.py
@@ -1,11 +1,7 @@
-# This is an example feature definition file
-
-from datetime import timedelta
 from pathlib import Path
+from datetime import timedelta
 
 import pandas as pd
-
-
 from feast import (
     Entity,
     FeatureService,
@@ -18,32 +14,41 @@ from feast import (
 from feast.on_demand_feature_view import on_demand_feature_view
 from feast.types import Float32, Float64, Int64
 
-# Define an entity for the driver. You can think of an entity as a primary key used to
-# fetch features.
+
+DATA_PATH = Path("data/driver_stats.parquet")
+
+__all__ = ["driver",
+           "driver_stats_source",
+           "driver_stats_feature_view",
+           "transformed_stats",
+           "driver_stats_push_source",
+           "driver_activity",
+           "driver_stats_fresh_feature_view"]
+
+
+# Define an entity for the driver.
+# You can think of an entity as a primary key used to fetch features.
 driver = Entity(name="driver", join_keys=["driver_id"])
 
-# Read data from parquet files. Parquet is convenient for local development mode. For
-# production, you can use your favorite DWH, such as BigQuery. See Feast documentation
-# for more info.
+# Read data from parquet files. Parquet is convenient for development mode.
+# For production, you can use your favorite DWH, such as BigQuery.
+# See Feast documentation for more info.
 driver_stats_source = FileSource(
     name="driver_hourly_stats_source",
-    path=f"{str(Path.home())}/user/Feast/data/driver_stats.parquet",
+    path=str(DATA_PATH),
     timestamp_field="event_timestamp",
     created_timestamp_column="created",
 )
 
-# Our parquet files contain sample data that includes a driver_id column, timestamps and
-# three feature column. Here we define a Feature View that will allow us to serve this
-# data to our model online.
-driver_stats_fv = FeatureView(
-    # The unique name of this feature view. Two feature views in a single
-    # project cannot have the same name
+# The dataset includes a `driver_id`, a timestamp and other feature columns.
+# Here we define a Feature View to serve this data to our model online.
+driver_stats_feature_view = FeatureView(
+    # The unique name of this feature view.
+    # Two feature views in a single project cannot have the same name.
     name="driver_hourly_stats",
     entities=[driver],
     ttl=timedelta(days=1),
-    # The list of features defined below act as a schema to both define features
-    # for both materialization of features into a store, and are used as references
-    # during retrieval for building a training dataset or serving features
+    # The list of features defined below define the db schema.
     schema=[
         Field(name="conv_rate", dtype=Float32),
         Field(name="acc_rate", dtype=Float32),
@@ -51,60 +56,38 @@ driver_stats_fv = FeatureView(
     ],
     online=True,
     source=driver_stats_source,
-    # Tags are user defined key/value pairs that are attached to each
-    # feature view
     tags={"team": "driver_performance"},
 )
 
-# Define a request data source which encodes features / information only
-# available at request time (e.g. part of the user initiated HTTP request)
-input_request = RequestSource(
-    name="vals_to_add",
-    schema=[
-        Field(name="val_to_add", dtype=Int64),
-        Field(name="val_to_add_2", dtype=Int64),
-    ],
-)
-
-
-# Define an on demand feature view which can generate new features based on
-# existing feature views and RequestSource features
-@on_demand_feature_view(
-    sources=[driver_stats_fv, input_request],
-    schema=[
-        Field(name="conv_rate_plus_val1", dtype=Float64),
-        Field(name="conv_rate_plus_val2", dtype=Float64),
-    ],
-)
-def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
-    return df
-
-
-# This groups features into a model version
-driver_activity_v1 = FeatureService(
-    name="driver_activity_v1",
-    features=[
-        driver_stats_fv[["conv_rate"]],  # Sub-selects a feature from a feature view
-        transformed_conv_rate,  # Selects all features from the feature view
-    ],
-)
-driver_activity_v2 = FeatureService(
-    name="driver_activity_v2", features=[driver_stats_fv, transformed_conv_rate]
-)
-
-# Defines a way to push data (to be available offline, online or both) into Feast.
+# Defines a way to push data (available offline, online or both) into Feast.
 driver_stats_push_source = PushSource(
     name="driver_stats_push_source",
     batch_source=driver_stats_source,
 )
 
-# Defines a slightly modified version of the feature view from above, where the source
-# has been changed to the push source. This allows fresh features to be directly pushed
-# to the online store for this feature view.
-driver_stats_fresh_fv = FeatureView(
+# Define an on demand feature view which can generate new features based on
+# existing feature views and RequestSource features.
+@on_demand_feature_view(
+    sources=[driver_stats_feature_view],
+    schema=[
+        Field(name="conv_plus_trips", dtype=Float64),
+        Field(name="acc_plus_trips", dtype=Float64)
+    ],
+)
+def transformed_stats(inputs: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame()
+    df["conv_plus_trips"] = inputs["conv_rate"] * inputs["avg_daily_trips"]
+    df["acc_plus_trips"] = inputs["acc_rate"] * inputs["avg_daily_trips"]
+    return df
+
+driver_activity = FeatureService(
+    name="driver_activity", features=[driver_stats_feature_view, transformed_stats]
+)
+
+# Defines a slightly modified version of the feature view from above, where the
+# source has been changed to the push source.
+# This allows fresh features to be directly pushed to the online store.
+driver_stats_fresh_feature_view = FeatureView(
     name="driver_hourly_stats_fresh",
     entities=[driver],
     ttl=timedelta(days=1),
@@ -116,26 +99,4 @@ driver_stats_fresh_fv = FeatureView(
     online=True,
     source=driver_stats_push_source,  # Changed from above
     tags={"team": "driver_performance"},
-)
-
-
-# Define an on demand feature view which can generate new features based on
-# existing feature views and RequestSource features
-@on_demand_feature_view(
-    sources=[driver_stats_fresh_fv, input_request],  # relies on fresh version of FV
-    schema=[
-        Field(name="conv_rate_plus_val1", dtype=Float64),
-        Field(name="conv_rate_plus_val2", dtype=Float64),
-    ],
-)
-def transformed_conv_rate_fresh(inputs: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
-    return df
-
-
-driver_activity_v3 = FeatureService(
-    name="driver_activity_v3",
-    features=[driver_stats_fresh_fv, transformed_conv_rate_fresh],
 )
