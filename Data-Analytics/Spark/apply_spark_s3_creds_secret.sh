e7587886e999683c2091d2510fe3e3380944c7ae
azubkov <anton.zubkov@hpe.com>
2023-10-25 12:53:21 +0300

    updated example with precondition to create secret in user namespace

--- Data-Analytics/Spark/apply_spark_s3_creds_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -e "s/\$AUTH_TOKEN/$AUTH_TOKEN/" /mnt/usr/ezua-tutorials/Data-Analytics/Spark/object_store_secret.yaml.tpl > /tmp/object_store_secret.yaml
-kubectl apply -f /tmp/object_store_secret.yaml
