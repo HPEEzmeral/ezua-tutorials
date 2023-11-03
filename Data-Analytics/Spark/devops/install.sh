8e575cf35efe76902bdab971f2e474bb9d4cc3ff
skandtandon <skand.tandon@hpe.com>
2023-11-03 15:10:14 +0530

    Updated Spark Analytics examples and Investment banking example

--- Data-Analytics/Spark/devops/install.sh
+++ Data-Analytics/Spark/devops/install.sh
@@ -10,17 +10,17 @@ do
 done
 
 SCRIPTPATH=$(dirname "${0}")
-spark_version=${spark_version:=3.4.0}
-APP_IMAGE_NAME=${APP_IMAGE_NAME:-"gcr.io/mapr-252711/ezaf-spark-demo-example"}
+spark_version=${spark_version:=3.4.1}
+APP_IMAGE_NAME=${APP_IMAGE_NAME:-"gcr.io/mapr-252711/3.4.1"}
 DOCKERFILE=${DOCKERFILE:-"${SCRIPTPATH}/../dockerfiles/SparkJarLocal-${spark_version}.Dockerfile"}
 
 if [[ $example == "mnist" ]]; then
-     APP_IMAGE_TAG=${APP_IMAGE_TAG:-"fy23-q3-mnist"}
+     APP_IMAGE_TAG=${APP_IMAGE_TAG:-"3.4.1"}
      SPARK_APPLICATION=${SPARK_APPLICATION:-"${SCRIPTPATH}/../k8s/DataTransferMnist-JarLocal-${spark_version}.yaml"}
      SRC_SPARK_JAR=${SPARK_JAR:-"${SCRIPTPATH}/../src/DataTransfer/DataTransfer.jar"}
      DEST_SPARK_JAR=${DEST_SPARK_JAR:-"/tmp/DataTransfer.jar"}
 elif [[ $example == "fts" ]]; then
-      APP_IMAGE_TAG=${APP_IMAGE_TAG:-"fy23-q3-fts"}
+      APP_IMAGE_TAG=${APP_IMAGE_TAG:-"3.4.1"}
       SPARK_APPLICATION=${SPARK_APPLICATION:-"${SCRIPTPATH}/../k8s/DataProcessTransferFts-JarLocal-${spark_version}.yaml"}
       SRC_SPARK_JAR=${SPARK_JAR:-"${SCRIPTPATH}/../src/DataProcessTransfer/DataProcessTransfer.jar"}
       DEST_SPARK_JAR=${DEST_SPARK_JAR:-"/tmp/DataProcessTransfer.jar"}
