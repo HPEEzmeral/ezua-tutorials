b9c1cb5614dc1952429de6183fffc5aaa6656e82
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2024-01-18 17:22:28 +0200

    Enhance Spark jobs for Airflow (#128)
    * Enhance Spark jobs for Airflow
    
    * Spark changes
    
    * Move files
    
    * Update path

--- Data-Engineering/Airflow/example_spark_pi.py
+++ Data-Engineering/Airflow/example_spark_pi.py
@@ -1,53 +1,56 @@
 from airflow import DAG
 from airflow.models.param import Param
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
+    SparkKubernetesSensor,
+)
 from airflow.utils.dates import days_ago
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 0
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "max_active_runs": 1,
+    "retries": 0,
 }
 
 dag = DAG(
-    'spark_pi',
+    "spark_pi",
     default_args=default_args,
     schedule_interval=None,
-    tags=['ezaf', 'spark', 'pi'],
+    tags=["ezaf", "spark", "pi"],
     params={
-        'airgap_registry_url': Param("", type=["null", "string"], pattern=r"^$|^\S+/$")
+        "airgap_registry_url": Param(
+            "",
+            type=["null", "string"],
+            pattern=r"^$|^\S+/$",
+            description="Airgap registry url. Trailing slash in the end is required",
+        ),
     },
     render_template_as_native_obj=True,
-    access_control={
-        'All': {
-            'can_read',
-            'can_edit',
-            'can_delete'
-        }
-    }
+    access_control={"All": {"can_read", "can_edit", "can_delete"}},
 )
 
 submit = SparkKubernetesOperator(
-    task_id='submit',
+    task_id="submit",
     application_file="example_spark_pi.yaml",
     do_xcom_push=True,
     dag=dag,
     api_group="sparkoperator.hpe.com",
-    enable_impersonation_from_ldap_user=True
+    enable_impersonation_from_ldap_user=True,
 )
 
 sensor = SparkKubernetesSensor(
-    task_id='monitor',
+    task_id="monitor",
     application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
     dag=dag,
     api_group="sparkoperator.hpe.com",
-    attach_log=True
+    attach_log=True,
 )
 
 submit >> sensor
