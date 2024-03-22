55d7a3cf61e3cf48fb654b9030fdaf5d19287846
Ashok Manda <ashokmanda@Ashoks-MacBook-Pro.local>
2024-03-22 21:21:24 +0530

    change makefile

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 APP_IMAGE_NAME=${APP_IMAGE_NAME:-"gcr.io/mapr-252711/ezua-tutorials"}
-APP_IMAGE_TAG=${APP_IMAGE_TAG:-"ezua-1.4-e235bdb"}
+GIT_HASH=$(git log -n1 --pretty=%h)
+APP_IMAGE_TAG="ezua-1.4-$GIT_HASH"
 
 function build() {
   echo "Building the ${APP_IMAGE_NAME}:${APP_IMAGE_TAG} image"
